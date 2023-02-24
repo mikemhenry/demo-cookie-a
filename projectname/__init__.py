bc62075bf152482ede651d7e15f3ac27bec045d5
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2023-02-24 07:58:23 -0700

    fixed things

--- projectname/__init__.py
+++ projectname/__init__.py
@@ -1,7 +1,7 @@
 """Test"""
 
 # Add imports here
-from .projectname import *
+from .projectname import *  # noqa: F401, F403
 
 
-from ._version import __version__
+from ._version import __version__  # noqa: F401
--- projectname/tests/test_projectname.py
+++ projectname/tests/test_projectname.py
@@ -12,4 +12,6 @@ import projectname
 
 def test_projectname_imported():
     """Sample test, will always pass so long as import statement worked."""
+    print("importing ", projectname.__name__)
+
     assert "projectname" in sys.modules
